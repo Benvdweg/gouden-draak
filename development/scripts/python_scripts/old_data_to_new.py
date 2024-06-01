8b2b4b74b06e60220c8bf46f85bc6a0652d85c2c
Benvdweg <b.vanderweg@student.avans.nl>
2024-06-01 11:40:09 +0200

    include price

--- development/scripts/python_scripts/old_data_to_new.py
+++ development/scripts/python_scripts/old_data_to_new.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 import pandas as pd
+import pymysql
 
 # Oude en nieuwe database connectiestrings voor MySQL/MariaDB
-old_db_conn_str = 'mysql+pymysql://root:@localhost:port/live.gouden_draak'
-new_db_conn_str = 'mysql+pymysql://root:@localhost:port/dev.gouden_draak'
+old_db_conn_str = 'mysql+pymysql://root:@localhost:3306/live.gouden_draak'
+new_db_conn_str = 'mysql+pymysql://root:@localhost:3306/dev.gouden_draak'
 
 # Verbinding maken met beide databases
 old_engine = create_engine(old_db_conn_str)
@@ -18,6 +19,7 @@ transformed_data = pd.DataFrame()
 transformed_data['id'] = old_data['id']
 transformed_data['name'] = old_data['naam']
 transformed_data['description'] = old_data['beschrijving']
+transformed_data['price'] = old_data['price']
 
 # Data invoegen in de nieuwe database
 transformed_data.to_sql('dishes', new_engine, if_exists='append', index=False)
