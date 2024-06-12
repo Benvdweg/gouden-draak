15aa8278dbdf7a95aaaeebe0a74f460fa2a3e0d0
Benvdweg <b.vanderweg@student.avans.nl>
2024-06-12 18:05:07 +0200

    init

---
+++ development/new_code/resources/views/layout/layout.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+    <title>The Golden Dragon</title>
+    <style>
+        body {
+            background-color: darkred;
+            margin: 15px;
+            margin-left: 50px;
+            margin-right: 50px
+        }
+
+        td {
+            padding: 0px;
+        }
+
+        @font-face {
+            font-family: 'chinese_takeawayregular';
+            src: url('{{asset('fonts/chinesetakeaway-webfont.woff2')}}') format('woff2'),
+            url('{{ asset('fonts/chinesetakeaway-webfont.woff') }}') format('woff');
+            font-weight: normal;
+            font-style: normal;
+        }
+
+        a {
+            text-decoration: none;
+            color: yellow;
+        }
+    </style>
+</head>
+
+    <body>
+        @yield('content')
+    </body>
+</html>
