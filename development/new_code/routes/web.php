7b0af716af89492e468672b1911ed7280cd32184
HesselVisser <130562101+HesselVisser@users.noreply.github.com>
2024-06-13 08:08:13 +0200

    Admin page created

--- development/new_code/routes/web.php
+++ development/new_code/routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/checkout', [CheckoutController::class, 'show'])->name('checkout');
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
 
