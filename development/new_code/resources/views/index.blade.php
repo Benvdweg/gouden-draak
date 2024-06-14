9505e403276ff980af183943421b4bb7650cdb88
HesselVisser <130562101+HesselVisser@users.noreply.github.com>
2024-06-14 22:28:19 +0200

    styling fix

--- development/new_code/resources/views/index.blade.php
+++ development/new_code/resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.layout')
 
 @section('content')
-    <div class="bg-[#ff0000] p-8">
+    <div class=" bg-red-500 p-8">
 
         <div class="p-4 mt-8">
             <div class="bg-[#fffaf0] border border-black p-2">
