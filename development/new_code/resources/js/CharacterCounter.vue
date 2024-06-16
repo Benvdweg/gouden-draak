0fbd7dcad06b449a4b1a5296255ca1c96fb1635f
Benvdweg <b.vanderweg@student.avans.nl>
2024-06-16 23:45:40 +0200

    vue (ik haat vue)

---
+++ development/new_code/resources/js/CharacterCounter.vue
@@ -0,0 +1,54 @@
+<!-- CharacterCounter.vue -->
+
+<template>
+    <div>
+        <textarea
+            v-model="content"
+            @input="updateCharacterCount"
+            name="message"
+            id="message"
+            class="w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring focus:ring-indigo-300 resize-none"
+            rows="5"
+            placeholder="Schrijf je bericht hier"
+        ></textarea>
+        <div class="text-right mt-2 text-gray-500">
+            Karakters: {{ characterCount }} / {{ maxCharacters }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        initialContent: {
+            type: String,
+            default: ''
+        },
+        maxCharacters: {
+            type: Number,
+            default: 4000
+        }
+    },
+    data() {
+        return {
+            content: this.initialContent,
+            characterCount: 0
+        };
+    },
+    watch: {
+        initialContent: function (newVal) {
+            this.content = newVal;
+            this.updateCharacterCount();
+        }
+    },
+    methods: {
+        updateCharacterCount() {
+            this.characterCount = this.content.length;
+        }
+    }
+};
+</script>
+
+<style scoped>
+/* Add any scoped styles here */
+</style>
