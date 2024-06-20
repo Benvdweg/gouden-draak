13d34b5236dcce585a0f406d6c976d6d20ee5c55
Benvdweg <b.vanderweg@student.avans.nl>
2024-06-20 22:35:17 +0200

    fix styling

--- development/new_code/resources/views/customer/contact.blade.php
+++ development/new_code/resources/views/customer/contact.blade.php
@@ -1,55 +1,25 @@
 @extends('layout.layout')
 
 @section('content')
-	<head>
-		<title>The Golden Dragon</title>
-		<style>
-			body {background-color: darkred; margin: 15px; margin-left: 50px; margin-right: 50px}
-			td {padding: 0px;}
-			
-		@font-face {
-			font-family: 'chinese_takeawayregular';
-			src: url('../fonts/chinesetakeaway-webfont.woff2') format('woff2'),
-				 url('../fonts/chinesetakeaway-webfont.woff') format('woff');
-			font-weight: normal;
-			font-style: normal;
-		}
 
-		a {text-decoration: none;
-		color: yellow;}
-		</style>
-	</head>
-	
-	<body>          
-    <table width=100%>
-						<tr style="padding-top:50px">
-							<td colspan="3" height="50px">	
-							</td>
-						</tr>
-						<tr style="padding-top:50px">
-							<td width="50px">	
-							</td>
-							<td align="center" style='font-size:5;border:1px solid black;background:floralwhite'> <br>
-								<h3>De Gouden Draak is eenvoudig te vinden, vlak bij het centrum, 5 minuten lopen achter het centraal station.
-								<br>
-								<br>
-									Onderwijsboulevard 215, kamer OG112<br> 5223 DE 's-Hertogenbosch	
-								</h3>
-							</td>
-							<td width="50px">	
-							</td>
-						</tr>
-						<tr style="padding-top:50px">
-							<td width="50px">	
-							</td>
-							<td>
-								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2473.432987711052!2d5.284448915590253!3d51.68852110526529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6ee8855f3c5d1%3A0x4c4797f27d227e73!2sOnderwijsboulevard%20215%2C%205223%20DE%20&#39;s-Hertogenbosch!5e0!3m2!1sen!2snl!4v1585055473064!5m2!1sen!2snl" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-							</td>
-							<td width="50px">	
-							</td>
-						</tr>
-					</table>
-	</body>
+    <div class="flex justify-center py-2">
+        <div class="w-full max-w-[94rem]">
+            <div class="bg-[#fffaf0] border border-black text-center">
+                <div class="font-bold font-times text-lg mt-8">
+                    <h3>De Gouden Draak is eenvoudig te vinden, vlak bij het centrum, 5 minuten lopen achter het centraal
+                        station. </h3>
+                    <br>
+                    <h3>Onderwijsboulevard 215, kamer OG112</h3>
+                    <h3 class="mb-3">5223 DE 's-Hertogenbosch</h3>
+                </div>
+            </div>
+            <div class="mt-0.5">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2473.432987711052!2d5.284448915590253!3d51.68852110526529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6ee8855f3c5d1%3A0x4c4797f27d227e73!2sOnderwijsboulevard%20215%2C%205223%20DE%20&#39;s-Hertogenbosch!5e0!3m2!1sen!2snl!4v1585055473064!5m2!1sen!2snl"
+                    width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
+                    tabindex="0"></iframe>
+            </div>
+        </div>
+    </div>
 
 @endsection
-
--- development/new_code/resources/views/layout/nav-bar.blade.php
+++ development/new_code/resources/views/layout/nav-bar.blade.php
@@ -3,26 +3,32 @@
     <div>
         <h1 class="text-[40px] text-[#ffff00] font-bold font-times">Chinees Indische Specialiteiten</h1>
         <h1 class="text-5xl text-[#ffff00] font-bold font-times">De Gouden Draak</h1>
-        <div class="mt-4 flex justify-center border border-t-[#808080] border-l-[#808080] border-r-[#2c2c2c] border-b-[#2c2c2c] p-0.5 space-x-1">
+        <div
+            class="mt-4 flex justify-center border border-t-[#808080] border-l-[#808080] border-r-[#2c2c2c] border-b-[#2c2c2c] p-0.5 space-x-1">
             <a href="{{ route('show.basic.menu') }}"
-               class="text-lg text-white flex-1 text-center border border-black bg-cover bg-no-repeat bg-menu-gradient font-times">Menukaart</a>
+               class="text-lg text-white flex-1 text-center border border-t-[#2c2c2c] border-l-[#2c2c2c] border-r-[#808080] border-b-[#808080] bg-cover bg-no-repeat bg-menu-gradient font-times">Menukaart</a>
             <a href="{{ route('customer.news') }}"
-               class="text-lg  text-white flex-1 text-center border border-black bg-cover bg-no-repeat bg-menu-gradient font-times">Nieuws</a>
+               class="text-lg  text-white flex-1 text-center border border-t-[#2c2c2c] border-l-[#2c2c2c] border-r-[#808080] border-b-[#808080] bg-cover bg-no-repeat bg-menu-gradient font-times">Nieuws</a>
             <a href="{{ route('customer.contact') }}"
-               class="text-lg text-white flex-1 text-center border border-black bg-cover bg-no-repeat bg-menu-gradient font-times">Contact</a>
+               class="text-lg text-white flex-1 text-center border border-t-[#2c2c2c] border-l-[#2c2c2c] border-r-[#808080] border-b-[#808080] bg-cover bg-no-repeat bg-menu-gradient font-times">Contact</a>
             <a href="{{ route('pick-up.menu-category-show') }}"
-               class="text-lg text-white flex-1 text-center border border-black bg-cover bg-no-repeat bg-menu-gradient font-times">Afhalen</a>
+               class="text-lg text-white flex-1 text-center border border-t-[#2c2c2c] border-l-[#2c2c2c] border-r-[#808080] border-b-[#808080] bg-cover bg-no-repeat bg-menu-gradient font-times">Afhalen</a>
         </div>
     </div>
     <img src="{{ asset('images/dragon-small-flipped.png') }}" alt="Dragon Head" class="w-52 mt-8 mr-2"/>
 </div>
-<div class="mt-8 mb-8">
-    <div class="flex flex-wrap justify-center gap-4">
-        @foreach($pages as $page)
-            <a href="{{ url($page->slug) }}"
-               class="text-lg text-white flex-1 text-center border border-black bg-cover bg-no-repeat bg-menu-gradient font-times">
-                {{ $page->title }}
-            </a>
-        @endforeach
-    </div>
+<div class="mb-8">
+    @if(!$pages->isEmpty())
+        <div class="flex justify-center">
+            <div
+                class="mt-2 flex justify-center border border-t-[#808080] border-l-[#808080] border-r-[#2c2c2c] border-b-[#2c2c2c] p-0.5 space-x-1">
+                @foreach($pages as $page)
+                    <a href="{{ url($page->slug) }}"
+                       class="text-lg text-white flex-1 text-center border border-t-[#2c2c2c] border-l-[#2c2c2c] border-r-[#808080] border-b-[#808080] bg-cover bg-no-repeat bg-menu-gradient font-times truncate max-w-fit px-4">
+                        {{ $page->title }}
+                    </a>
+                @endforeach
+            </div>
+        </div>
+    @endif
 </div>
