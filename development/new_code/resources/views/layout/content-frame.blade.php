363039e069e4d728371d2de0e78bfa15c686c461
Benvdweg <b.vanderweg@student.avans.nl>
2024-06-20 17:28:31 +0200

    update

--- development/new_code/resources/views/layout/content-frame.blade.php
+++ development/new_code/resources/views/layout/content-frame.blade.php
@@ -1,99 +1,101 @@
 <table id="main_table" class="p-5 w-full border-collapse">
-<tr class="h-[7px] bg-red-500">
-    <td colspan="9"></td>
-</tr>
+    <tr class="h-[7px] bg-[#ff0000]">
+        <td colspan="9">
+            @include('components.top-bar')
+        </td>
+    </tr>
 
     <tr>
-    <tr class="h-6 bg-red-500">
-    <td class="w-1"></td>
-    <td class="w-6 border-l-4 border-t-4 border-yellow-500"></td>
-    <td class="w-6 border-r-4 border-t-4 border-yellow-500"></td>
-    <td class="w-6 border-r-4 border-b-4 border-yellow-500"></td>
-    <td class="border-t-4 border-b-4 border-yellow-500"></td>
-    <td class="w-6 border-l-4 border-b-4 border-yellow-500"></td>
-    <td class="w-6 border-l-4 border-t-4 border-yellow-500"></td>
-    <td class="w-6 border-r-4 border-t-4 border-yellow-500"></td>
-    <td class="w-1"></td>
-</tr>
+    <tr class="h-[25px] bg-[#ff0000]">
+        <td class="w-1"></td>
+        <td class="w-[29px] border-l-4 border-t-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-r-4 border-t-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-r-4 border-b-4 border-[#ffff00]"></td>
+        <td class="border-t-4 border-b-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-l-4 border-b-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-l-4 border-t-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-r-4 border-t-4 border-[#ffff00]"></td>
+        <td class="w-1"></td>
+    </tr>
 
-<tr class="h-6 bg-red-500">
-    <td class="w-1"></td>
-    <td class="w-6 border-l-4 border-b-4 border-yellow-500"></td>
-    <td class="w-6 border-4 border-yellow-500"></td>
-    <td class="w-6 border-4 border-yellow-500"></td>
-    <td></td>
-    <td class="w-6 border-4 border-yellow-500"></td>
-    <td class="w-6 border-4 border-yellow-500"></td>
-    <td class="w-6 border-r-4 border-b-4 border-yellow-500"></td>
-    <td class="w-1"></td>
-</tr>
+    <tr class="h-[25px] bg-[#ff0000]">
+        <td class="w-1"></td>
+        <td class="w-[29px] border-l-4 border-b-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-4 border-[#ffff00]"></td>
+        <td></td>
+        <td class="w-[29px] border-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-r-4 border-b-4 border-[#ffff00]"></td>
+        <td class="w-1"></td>
+    </tr>
 
-<tr class="h-[25px] bg-red-500">
-  <td class="w-[7px]"></td>
-  <td class="w-[25px] border-r-4 border-b-4 border-yellow-500"></td>
-  <td class="w-[25px] border-4 border-yellow-500"></td>
-  <td class="w-[25px]"></td>
-  <td></td>
-  <td class="w-[25px]"></td>
-  <td class="w-[25px] border-4 border-yellow-500"></td>
-  <td class="w-[25px] border-b-4 border-yellow-500"></td>
-  <td class="w-[7px]"></td>
-</tr>
+    <tr class="h-[25px] bg-[#ff0000]">
+        <td class="w-[7px]"></td>
+        <td class="w-[29px] border-r-4 border-b-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-4 border-[#ffff00]"></td>
+        <td class="w-[29px]"></td>
+        <td></td>
+        <td class="w-[29px]"></td>
+        <td class="w-[29px] border-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-b-4 border-[#ffff00]"></td>
+        <td class="w-[7px]"></td>
+    </tr>
 
-<tr class="h-[50px] bg-red-500">
-  <td class="w-[7px]"></td>
-  <td class="w-[25px] border-r-4 border-l-4 border-yellow-500"></td>
-  <td class="w-[25px]"></td>
-  <td class="w-[25px]"></td>
-  <td class="text-center">
-    @yield('inside-frame')
-    <br>
-  </td>
-  <td class="w-[25px]"></td>
-  <td class="w-[25px]"></td>
-  <td class="w-[25px] border-r-4 border-l-4 border-yellow-500"></td>
-  <td class="w-[7px]"></td>
-</tr>
+    <tr class="h-[50px] bg-[#ff0000]">
+        <td class="w-[7px]"></td>
+        <td class="w-[29px] border-r-4 border-l-4 border-[#ffff00]"></td>
+        <td class="w-[29px]"></td>
+        <td class="w-[29px]"></td>
+        <td class="text-center">
+            @yield('inside-frame')
+            <br>
+        </td>
+        <td class="w-[29px]"></td>
+        <td class="w-[29px]"></td>
+        <td class="w-[29px] border-r-4 border-l-4 border-[#ffff00]"></td>
+        <td class="w-[7px]"></td>
+    </tr>
 
-<tr class="h-[25px] bg-red-500">
-  <td class="w-[7px]"></td>
-  <td class="w-[25px] border-r-4 border-t-4 border-yellow-500"></td>
-  <td class="w-[25px] border-4 border-yellow-500"></td>
-  <td class="w-[25px]"></td>
-  <td></td>
-  <td class="w-[25px]"></td>
-  <td class="w-[25px] border-4 border-yellow-500"></td>
-  <td class="w-[25px] border-t-4 border-yellow-500"></td>
-  <td class="w-[7px]"></td>
-</tr>
+    <tr class="h-[25px] bg-[#ff0000]">
+        <td class="w-[7px]"></td>
+        <td class="w-[29px] border-r-4 border-t-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-4 border-[#ffff00]"></td>
+        <td class="w-[29px]"></td>
+        <td></td>
+        <td class="w-[29px]"></td>
+        <td class="w-[29px] border-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-t-4 border-[#ffff00]"></td>
+        <td class="w-[7px]"></td>
+    </tr>
 
-<tr class="h-[25px] bg-red-500">
-  <td class="w-[7px]"></td>
-  <td class="w-[25px] border-l-4 border-t-4 border-yellow-500"></td>
-  <td class="w-[25px] border-4 border-yellow-500"></td>
-  <td class="w-[25px] border-4 border-yellow-500"></td>
-  <td></td>
-  <td class="w-[25px] border-4 border-yellow-500"></td>
-  <td class="w-[25px] border-4 border-yellow-500"></td>
-  <td class="w-[25px] border-r-4 border-t-4 border-yellow-500"></td>
-  <td class="w-[7px]"></td>
-</tr>
+    <tr class="h-[25px] bg-[#ff0000]">
+        <td class="w-[7px]"></td>
+        <td class="w-[29px] border-l-4 border-t-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-4 border-[#ffff00]"></td>
+        <td></td>
+        <td class="w-[29px] border-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-r-4 border-t-4 border-[#ffff00]"></td>
+        <td class="w-[7px]"></td>
+    </tr>
 
 
-<tr class="h-6 bg-red-500">
-    <td class="w-1"></td>
-    <td class="w-6 border-l-4 border-b-4 border-yellow-500"></td>
-    <td class="w-6 border-r-4 border-b-4 border-yellow-500"></td>
-    <td class="w-6 border-r-4 border-yellow-500"></td>
-    <td class="border-t-4 border-b-4 border-yellow-500"></td>
-    <td class="w-6 border-l-4 border-yellow-500"></td>
-    <td class="w-6 border-l-4 border-b-4 border-yellow-500"></td>
-    <td class="w-6 border-r-4 border-b-4 border-yellow-500"></td>
-    <td class="w-1"></td>
-</tr>
+    <tr class="h-[25px] bg-[#ff0000]">
+        <td class="w-1"></td>
+        <td class="w-[29px] border-l-4 border-b-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-r-4 border-b-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-r-4 border-[#ffff00]"></td>
+        <td class="border-t-4 border-b-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-l-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-l-4 border-b-4 border-[#ffff00]"></td>
+        <td class="w-[29px] border-r-4 border-b-4 border-[#ffff00]"></td>
+        <td class="w-1"></td>
+    </tr>
 
-<tr class="h-1 bg-red-500">
-    <td colspan="9"></td>
-</tr>
+    <tr class="h-1 bg-[#ff0000]">
+        <td colspan="9"></td>
+    </tr>
 
 </table>
